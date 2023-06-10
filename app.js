4f44c1d8af75230a6cc3cc609ae96da55fb6d288
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-06-10 11:19:44 +0530

    Lets Revise Javascript concepts

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+product = (a, b) => {
+	return a * b;
+};
+
+product(2, 3);
+
+let Student = {
+	firstName: "Sahil",
+	rollNo: "2275",
+	lastName: "Kumar",
+
+	full_name() {
+		console.log(`${this.firstName} ${this.lastName}`);
+	},
+};
+Student.full_name();
