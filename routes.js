9acdb45ffad5e24cffa6b6882652cf07d4557290
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-06-11 00:22:51 +0530

    rout handling by exporting functions

---
+++ routes.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+
+let routesHandler =(req,res)=>{
+    let url = req.url;
+    let method = req.method;
+    if (url == "/") {
+        let fileData = fs.readFile("target.txt", "utf-8", (err, data) => {
+            if (err) console.log(err);
+            else {
+                res.write(`<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Home</title>
+            </head>
+            <body>
+            <p>${data}</p>
+                <form action="/msg" method = "POST">
+                    <label for="name"> Enter your text</label>
+                    <input type="text" id = "name" name = "msg">
+                    <button type="submit">Send</button>
+                </form>
+                
+            </body>
+            </html>`);
+                res.end();
+            }
+        });
+    } else if (url == "/msg") {
+        let newArr = [];
+        req.on("data", (chunk) => {
+            newArr.push(chunk);
+        });
+        req.on("end", () => {
+            let tempdata = Buffer.concat(newArr).toString();
+            let data = tempdata.split("=")[1];
+            fs.writeFile("target.txt", data, (err) => {
+                if (err) console.log(err);
+                else {
+                    res.statusCode = 302;
+                    res.setHeader("Location", "/");
+                    res.end("got here");
+                }
+            });
+        });
+    } else {
+        res.end("wrong url");
+    }
+    
+}
+
+module.exports = {
+    routesHandler
+}
